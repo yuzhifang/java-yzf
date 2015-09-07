cef3d51d3d7f9b13ec3b5f014ffd8dca59d576b9
Administrator <Administrator@WIN-9HABFKVITNF>
2015-09-07 17:23:23 +0800

    项目提交

---
+++ src/heihei/java/heiheiTest.java
@@ -0,0 +1,19 @@
+package heihei.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class heiheiTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
